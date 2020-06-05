aac3f07c68e17b8b7f2eb5f12ed18a6997f55a08
Gabriel Trevisan <gabriel.trevisan.santos@gmail.com>
2020-06-05 14:11:40 -0300

    Updates

---
+++ precache-manifest.f0223feff86fcd9886b1a3701a3348ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d657c5bc0a723f7b739fb61930c15406",
+    "url": "/contacts/index.html"
+  },
+  {
+    "revision": "bfe1423a43002d9d256e",
+    "url": "/contacts/static/css/main.36367b24.chunk.css"
+  },
+  {
+    "revision": "65506f8e9e293d5a2b34",
+    "url": "/contacts/static/js/2.c00676e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contacts/static/js/2.c00676e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfe1423a43002d9d256e",
+    "url": "/contacts/static/js/main.4631d4a5.chunk.js"
+  },
+  {
+    "revision": "2f2091dcb801da4800c3",
+    "url": "/contacts/static/js/runtime-main.43a2b114.js"
+  },
+  {
+    "revision": "d34eeea3c5b1d745e369382dbef70bae",
+    "url": "/contacts/static/media/logo.d34eeea3.svg"
+  }
+]);
